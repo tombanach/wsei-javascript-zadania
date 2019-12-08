b0a3dc37489a91853bb33765e6f0cfe6304ed2b5
tombanach <tom.banach98@gmail.com>
2019-12-08 12:34:04 +0100

    a

---
+++ 1_Zadania/obowiązkowe/7_Wiecej_o_funkcjach/js/zadanie03.js
@@ -0,0 +1,17 @@
+a();
+
+function a() {
+
+    console.log("Cześć")
+}
+
+a();
+
+b();
+
+var b = function(){
+
+    console.log("Witaj");
+}
+
+b();
--- 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
+++ 1_Zadania/obowiązkowe/8_DOM_Wyszukiwanie_elementow/js/app.js
@@ -2,5 +2,38 @@ document.addEventListener("DOMContentLoaded", function () {
     /*
     Poniżej napisz kod rozwiązujący zadania. Odpowiedzi oddzielaj komentarzami.
      */
+    const titleEl = document.querySelector("div .title");
 
+    function getDataAnimation(element){
+        return element.getAttribute("data-animation");
+    }
+
+    console.log(getDataAnimation(titleEl));
+
+
+    // Zadanie 1
+
+    const home = document.querySelector("#home");
+    const home2 = document.getElementById("home");
+    const lielement = document.querySelector('li[data-direction]');
+    const element = document.querySelector(".block");
+
+
+    // Zadanie 2
+
+    const list = document.querySelectorAll("nav li");
+    const paragraphs = document.querySelectorAll("div p");
+    const divs = document.querySelectorAll("article div");
+
+    console.log(list);
+    console.log(paragraphs);
+    console.log(divs);
+
+
+    // Zadanie 3
+
+    const article = document.querySelector("article.first");
+    let result = article.getElementsByTagName("h1");
+    
+    console.log(result.length);
 });
